5973d1ea943307a8c7c703dded2464f733d44e73
Shavinda <Shavinda@DESKTOP-9DOQ4CM>
2021-12-12 16:51:33 +0530

    Add project files.

---
+++ Inventory Control System/Migrations/20211212090001_productcategory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inventory_Control_System.Migrations
+{
+    public partial class productcategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductCategoryDetails",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CategoryDescription = table.Column<string>(type: "nvarchar(1000)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategoryDetails", x => x.CategoryID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductCategoryDetails");
+        }
+    }
+}
---
+++ Inventory Control System/Migrations/20211212092543_productpage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inventory_Control_System.Migrations
+{
+    public partial class productpage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductDetails",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    ProductDescription = table.Column<string>(type: "nvarchar(1000)", nullable: true),
+                    ProductPrice = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    ProductQuantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductDetails", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductDetails");
+        }
+    }
+}
